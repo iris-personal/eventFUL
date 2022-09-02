4ce0fc06b6d60bc9ae0433ef5073c188566278da
iris-personal <mary.iris.toland@gmail.com>
2022-09-02 13:40:42 -0500

    Edit Landing Page

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import './Logo.css';
+
+export default function Logo() {
+  return (
+    <div className="Logo">
+      <div>eventFUL</div>
+    </div>
+  );
+}
--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react';
+import './AuthPage.css'; 
 import SignUpForm from '../../components/SignUpForm/SignUpForm';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import Logo from '../../components/Logo/Logo';
 
 export default function AuthPage({ setUser }) {
   const [showLogin, setShowLogin] = useState(true);
   
   return (
-    <main>
-      <h1>AuthPage</h1>
-      <button onClick={() => setShowLogin(!showLogin)}>
-        {showLogin ? 'Sign Up' : 'Log In'}
-      </button>
-      { showLogin ?
-          <LoginForm setUser={setUser} />
-          :
-          <SignUpForm setUser={setUser} />
-      }
+    <main className="AuthPage">
+      <div>
+        <Logo />
+        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      </div>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
     </main>
   );
 }
