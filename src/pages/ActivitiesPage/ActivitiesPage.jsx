ebb185c434e02e0a1e49937902eee0e68328cb56
iris-personal <mary.iris.toland@gmail.com>
2022-09-06 18:48:56 -0500

    Activities Page Update

--- src/pages/ActivitiesPage/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage/ActivitiesPage.jsx
@@ -1,6 +1,6 @@
 import { checkToken } from '../../utilities/users-service';
 
-export default function OrderHistoryPage() {
+export default function ActivitiesPage() {
   
   async function handleCheckToken() {
     const expDate = await checkToken();
