4c2a8a147ec1f391aa2f002b07aa9c1104b01fe8
iris-personal <mary.iris.toland@gmail.com>
2022-09-06 17:00:59 -0500

    Add responsive calendar

--- src/pages/CalendarPage/CalendarPage.jsx
+++ src/pages/CalendarPage/CalendarPage.jsx
@@ -1,7 +1,16 @@
-import { ResponsiveCalendar } from 'react-responsive-calendar'
+import React, { useState } from "react";
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
+import "./CalendarPage.css"
 
 export default function CalnedarPage() {
-  return(
-  <ResponsiveCalendar />
+  const [selectedDay, setSelectedDay] = useState(null);
+  return (
+    <Calendar
+      value={selectedDay}
+      onChange={setSelectedDay}
+      calendarClassName="responsive-calendar" // added this
+      shouldHighlightWeekends
+    />
   );
 }
