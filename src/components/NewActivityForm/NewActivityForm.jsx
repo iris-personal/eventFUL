4b21a6407f2ea68052d391578c20cba47f7b121f
iris-personal <mary.iris.toland@gmail.com>
2022-09-09 12:11:58 -0500

    Create Add Activity Function

--- src/components/NewActivityForm/NewActivityForm.jsx
+++ src/components/NewActivityForm/NewActivityForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 
-export default function ActivityForm({ addActivity }) {
+export default function NewActivityForm({ addActivity }) {
     const [newActivity, setNewActivity] = useState({
         date: "",
         time: "",
--- src/pages/ActivitiesPage/ActivitiesPage.jsx
+++ src/pages/ActivitiesPage/ActivitiesPage.jsx
@@ -4,6 +4,7 @@ import './ActivitiesPage.css';
 import * as activitiesAPI from "../../utilities/activities-api";
 import Logo from '../../components/Logo/Logo';
 import UserLogOut from '../../components/UserLogOut/UserLogOut';
+import NewActivityForm from '../../components/NewActivityForm/NewActivityForm';
 
 export default function ActivitiesPage({ user, setUser, activity, setActivity }) {
 useEffect(function() {
@@ -13,6 +14,9 @@ useEffect(function() {
   }
   getActivities();
 }, []);
+function addActivity(activity) {
+  setActivity([...activities, activity]);
+}
   return (
     <main className="ActivitiesPage">
       <aside>
@@ -22,6 +26,7 @@ useEffect(function() {
       </aside>
       {/* Render an Day or CalendarDate component (needs to be coded) */}
       {/* Render AddActivity component */}
+      <NewActivityForm addActivity={addActivity} />
 
     </main>
   );
